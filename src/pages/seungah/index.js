98f8148bd38e61d484e25847230f14c22c0eaf7f
seung07-prog <bsa070423@naver.com>
2024-10-25 15:50:27 +0900

    fff

--- src/pages/seungah/index.js
+++ src/pages/seungah/index.js
@@ -8,15 +8,15 @@ function SeungAhPage() {
                 <Container>
                     <MyPhotoBox>
                         {/*나의 사진 혹은 내가 좋아하는 사진 넣어주기~*/}
-                        <img src={"https://i.pinimg.com/control/564x/58/69/d6/5869d65fce581f894d44dcf2807332ef.jpg"}/>
+                        <img src={"https://i.pinimg.com/736x/de/48/12/de48125fedb8b893ad1852393ab14946.jpg"}/>
                     </MyPhotoBox>
                     <ContentsBox>
                         <PersonalData>
-                            <h1>이름</h1>
-                            <h2>생년월일</h2>
-                            <h2>MBTI</h2>
-                            <h2>학교</h2>
-                            <h2>인스타그램</h2>
+                            <h1>백승아</h1>
+                            <h2>07.04.23</h2>
+                            <h2>ENFP</h2>
+                            <h2>풍생고</h2>
+                            <h2>white.winah</h2>
                         </PersonalData>
                     </ContentsBox>
                 </Container>
@@ -29,7 +29,7 @@ function SeungAhPage() {
 export default SeungAhPage;
 
 const Wrapper = styled.div`
-    background: lightblue;
+    background: lightpink;
     width: 100%;
     height: 100%;
     display: flex;
