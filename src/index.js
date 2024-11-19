98a63ae2c2ad04ee7ebb0a721726edde8976a7f7
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-11-19 16:28:07 +0900

    feat: ThemeProvider 추가 스타일링 전역관리

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
-  
   </React.StrictMode>
 );
 
---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    logo: "#43509B",
+    logoHover:'#8B96D6',
+    font: "#616161",
+    naviFont: "#FFFFFF",
+
+  },
+  backgroundColors: {
+    page: "#FFFFFF",
+    navigation:'#43509B',
+  },
+
+  mobile: `430px`,
+  tablet: "820px",
+};
+
+export default theme;
