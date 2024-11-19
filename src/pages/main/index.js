ea244c968e894d51022f87d7fe629ef8c1bc9100
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-11-19 16:28:55 +0900

    css: 메인 페이지 스타일 변경

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,50 +1,60 @@
 import styled from "styled-components";
-
+import { TouchIcon } from "../../assets/icons";
 
 function MainPage() {
-
-    return (
-        <Wrapper>
-            <ImgBox>
-                <img src={'https://i.pinimg.com/control/564x/24/37/bb/2437bbd6e6330d20dac79146add7e8f7.jpg'}/>
-            </ImgBox>
-            <ItemBox>
-                <h1>메인 페이지입니다 ٩( ᐛ )و </h1>
-            </ItemBox>
-        {/*    여기에 서연이랑 승아 페이지로 이동할 수 있는 버튼을 만들어 주세요!! */}
-
-        </Wrapper>
-
-    );
+  return (
+    <>
+      <Wrapper>
+        <Contents>
+          <p>같이 공부해요</p>
+          <ImgBox>
+            <img
+              src={
+                "https://i.pinimg.com/736x/2c/02/31/2c02317e875c7253f09f7827e297cbb5.jpg"
+              }
+            />
+          </ImgBox>
+        </Contents>
+      </Wrapper>
+    </>
+  );
 }
 
 export default MainPage;
+
 const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @font-face {
+    font-family: "RiaSans-ExtraBold";
+    src: url("https://fastly.jsdelivr.net/gh/projectnoonnu/2410-1@1.0/RiaSans-ExtraBold.woff2")
+      format("woff2");
+    font-weight: normal;
+    font-style: normal;
+  }
+  p {
+    display: flex;
+    justify-content: center;
+    font-family: "RiaSans-ExtraBold";
+    font-size: 30px;
+    margin: 20px;
+    color: ${({ theme }) => theme.colors.font};
+  }
+  span {
+    display: flex;
+    justify-content: center;
+    padding: 10px;
+  }
 `;
 
-
+const Contents = styled.div``;
 
 const ImgBox = styled.div`
-   img{
-       border-radius: 20px;   
-   }
-`
-
-
-const ItemBox = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    @font-face {
-        font-family: 'HSSanTokki20-Regular';
-        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2405@1.0/HSSanTokki20-Regular.woff2') format('woff2');
-        font-weight: normal;
-        font-style: normal;
-    }
-    h1{
-        color: white;
-        font-family: 'HSSanTokki20-Regular';
-        
-    }
-`
-
+  img {
+    border-radius: 20px;
+    height: 300px;
+  }
+`;
